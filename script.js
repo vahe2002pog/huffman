ebce32e48a6db33796a668bfa5ba664f29250571
ProgerX <vahepogosyan@gmail.com>
2019-06-08 11:06:25 +0500

    Вывод в таблицу и текстовое поле, немного дерева

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
 function Calculate(){
     let str = textValue = $("#textBox").val();
     let arr = [];
-    let l = 0;
     while(str.length > 0){
         let s = str[0];
         let o = str.match(new RegExp(`${s}`,"g") || []).length;
@@ -13,7 +11,18 @@ function Calculate(){
         for(o; o > 0; o--)
             str = str.replace(s,'');
     }
+    getCodes(arr);
+    mySort(arr);
+    $('#textArea').val(codingText(textValue,arr));
 
+    $("#tableInfo").find("tr:gt(0)").remove();
+    for(let i = 0; i < arr.length; i++){
+        $('#tableInfo').append(`<tr><td>${arr[i].char}</td><td>${arr[i].val}</td><td>${arr[i].code}</td></tr>`);
+    }
+}
+
+drawTree();
+function getCodes(arr){
     let arrClone = [...arr];
 
     while (arrClone.length > 1) {
@@ -25,29 +34,58 @@ function Calculate(){
         }
         arrClone.shift();
         arrClone[0] = res;
-        res.ch1.code = '0';
-        res.ch2.code = '1';
+        res.ch1.num = '0';
+        res.ch2.num = '1';
         res.ch1.parent = arrClone[0];
         res.ch2.parent = arrClone[0];
     }
 
-    let codes = [];
     for (let i = 0; i < arr.length; i++) {
         let code = '';
         let ptr = arr[i];
         while(ptr.parent){
-            code = ptr.code + code;
+            code = ptr.num + code;
             ptr = ptr.parent;
         }
-        codes.push({
-            char: arr[i].char,
-            code: code
-        })        
+        arr[i].code = code;      
     }
 }
-
 function mySort(_arr){
     _arr.sort((a, b) => {
         return a.val - b.val;
     });
+}
+function codingText(text,arr){
+    let str = "";
+    for(let i = 0; i < text.length; i++){
+        str +=  arr[getIndexCon(text[i],arr)].code;
+    }
+    return str;
+}
+function getIndexCon(ch,arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].char == ch)
+        {
+            return i;
+        }
+    }
+}
+function drawTree(arr){
+    let Canvas = $("#canvasTree").get(0);
+    let ctx = Canvas.getContext("2d");
+
+    for(let i = 1;i<10;i++){
+        ctx.beginPath();
+        ctx.arc(700 + i*50, i*80, 15, 0, 2 * Math.PI);        
+        ctx.moveTo(700 + i*50, i*80-15);        
+        ctx.lineTo(700 + (i-1)*50+15, (i-1)*80-10 );
+        ctx.stroke();
+        
+        ctx.beginPath();        
+        ctx.arc(700 + (-i)*50, i*80, 15, 0, 2 * Math.PI);        
+        ctx.moveTo(700 + (-i)*50, i*80-15);        
+        ctx.lineTo(700 + ((-i)+1)*50, (i-1)*80-10 );
+        ctx.stroke();
+    }
+    
 }
